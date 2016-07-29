aeabf4130fed036bd5f1b4f35d9aa783dc3fd1bb
Liam Goodacre <goodacre.liam@gmail.com>
2016-07-29 01:28:07 +0100

    Revised plan - no-longer subclassing filterable/witherable

--- src/Data/Stitchable.purs
+++ src/Data/Stitchable.purs
@@ -1,24 +1,23 @@
 module Data.Stitchable
   ( class Stitchable
-  , wiltStitch
-  , witherStitch
-  , stitchWith
+  , stitchedWith
+  , stitched
+  , stitch
+  , module Data.Zippable
   ) where
 
 import Control.Applicative (class Applicative, pure)
-import Data.Zippable (class Zippable)
-import Data.Witherable (class Witherable)
-import Data.Either (Either())
-import Data.Maybe (Maybe())
+import Data.Zippable (class Zippable, zipped)
+import Data.Functor ((<$>))
+import Data.Filterable (class Filterable, filtered)
+import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple(..))
-import Data.These (These())
+import Data.These (These(..))
 
 -- | `Stitchable` represents data structures which support being _zipped_
 -- | together with effects in some `Applicative` functor.
 -- |
--- | - `wiltStitch` - TODO
--- | - `witherStitch` - TODO
--- | - `stitchWith` - TODO
+-- | - `stitchedWith` - TODO
 -- |
 -- | Laws:
 -- |
@@ -31,18 +30,22 @@ import Data.These (These())
 -- | Default implementations are provided by the following functions:
 -- |
 -- | - TODO
-class (Zippable t, Witherable t) <= Stitchable t where
-  wiltStitch :: forall m x y l r. Applicative m =>
-    (These x y -> m (Either l r)) -> t x -> t y -> m { left :: t l
-                                                     , right :: t r }
+class Zippable f <= Stitchable f where
+  stitchedWith :: forall m x y o.
+    (These x y -> m o) -> f x -> f y -> m (f o)
 
-  witherStitch :: forall m x y o. Applicative m =>
-    (These x y -> m (Maybe o)) -> t x -> t y -> m (t o)
+stitched :: forall f m x y. (Zippable f, Applicative m) =>
+  f x -> f y -> m (f (These x y))
+stitched xs ys = pure (zipped xs ys)
 
-  stitchWith :: forall m x y o. Applicative m =>
-    (x -> y -> m o) -> t x -> t y -> m (t o)
+stitchWith :: forall f m x y o. (Stitchable f, Filterable f, Applicative m) =>
+  (x -> y -> m o) -> f x -> f y -> m (f o)
+stitchWith f xs ys = filtered <$> (stitchedWith applyOnBoth xs ys) where  
+  applyOnBoth (This x) = pure Nothing
+  applyOnBoth (That y) = pure Nothing
+  applyOnBoth (Both x y) = Just <$> f x y
 
-stitch :: forall m t x y. (Stitchable t, Applicative m) =>
-  t x -> t y -> m (t (Tuple x y))
+stitch :: forall f m x y. (Stitchable f, Filterable f, Applicative m) =>
+  f x -> f y -> m (f (Tuple x y))
 stitch = stitchWith (\x y -> pure (Tuple x y))
 
--- src/Data/Zippable.purs
+++ src/Data/Zippable.purs
@@ -1,22 +1,20 @@
 module Data.Zippable
   ( class Zippable
-  , partitionZip
-  , filterZip
+  , zippedWith
+  , zipped
   , zipWith
   , zip
   ) where
 
-import Data.Filterable (class Filterable)
-import Data.Maybe (Maybe())
-import Data.Either (Either())
+import Control.Category ((<<<), id)
+import Data.Filterable (class Filterable, filtered)
+import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple(..))
-import Data.These (These())
+import Data.These (These(..))
 
 -- | `Zippable` represents data structures which support being _zipped_ together.
 -- |
--- | - `partitionZip` - TODO
--- | - `filterZip` - TODO
--- | - `zipWith` - TODO
+-- | - `zippedWith` - TODO
 -- |
 -- | Laws:
 -- | - TODO
@@ -24,18 +22,22 @@ import Data.These (These())
 -- | Default implementations are provided by the following functions:
 -- |
 -- | - TODO
-class Filterable f <= Zippable f where
-  partitionZip :: forall x y l r.
-    (These x y -> Either l r) -> f x -> f y -> { left :: f l
-                                               , right :: f r }
+class Zippable f where
+  zippedWith :: forall x y o.
+    (These x y -> o) -> f x -> f y -> f o
 
-  filterZip :: forall x y o.
-    (These x y -> Maybe o) -> f x -> f y -> f o
+zipped :: forall f x y. Zippable f =>
+  f x -> f y -> f (These x y)
+zipped = zippedWith id
 
-  zipWith :: forall x y o.
-    (x -> y -> o) -> f x -> f y -> f o
+zipWith :: forall f x y o. (Zippable f, Filterable f) =>
+  (x -> y -> o) -> f x -> f y -> f o
+zipWith f xs ys = filtered (zippedWith applyOnBoth xs ys) where  
+  applyOnBoth (This x) = Nothing
+  applyOnBoth (That y) = Nothing
+  applyOnBoth (Both x y) = Just (f x y)
 
-zip :: forall f x y. Zippable f =>
+zip :: forall f x y. (Filterable f, Zippable f) =>
   f x -> f y -> f (Tuple x y)
 zip = zipWith Tuple
 
